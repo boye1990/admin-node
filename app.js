d70186de110f5262cd7539f7c78126c5e1a5e972
boye <boye_1990@aliyun.com>
2019-12-24 13:38:41 +0800

    在app.js中间添加一个mylogger中间件

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.get('/', function(req, res) {
     res.send('hello node')
 })
 
+/**
+ * 中间件是一个函数，在请求和响应周期中被顺序调用
+ */
+// 创建中间件
+const myLogger = function (req, res, next) {
+    console.log('myLogger')
+    next()
+}
+
+// 使用中间件， 中间件需要在响应结束前被调用
+app.use(myLogger)
+
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {
     const { address, port } = server.address()
