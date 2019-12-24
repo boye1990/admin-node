154331cce0622eabc9fe3d57e6eeeeba59c7b6f3
boye <boye_1990@aliyun.com>
2019-12-24 16:50:19 +0800

    项目框架搭建，添加全局router，utils工具文件

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const router = require('./router'); // router是我们自定义的一个中间件
 
 // 创建 express 应用
 const app = express();
+
+// 在app.js中托管router
+app.use('/', router)
 
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {
---
+++ router/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const boom = require('boom');
+
+const userRouter = require('./user');
+
+const { CODE_ERROR } = require('../utils/constant');
+
+// 注册路由
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send('欢迎来到博烨空间');
+});
+
+// 通过 userRouter 来处理 /user 路由， 对路由进行解耦。 接口user开头的路径
+router.use('/user', userRouter);
+
+/**
+ * 集中处理404请求的中间件
+ * 注意：该中间必须放在正常处理流程之后
+ * 否则， 会拦截正常请求
+ */
+router.use((req, res, next) => {
+    next(boom.notFound('接口不存在'));
+});
+
+/**
+ * 自定义路由异常处理中间件
+ * 注意两点：
+ * 第一，方法参数不能少
+ * 第二，方法必须放在路由最后
+ */
+router.use((err, req, res, next) => {
+    const msg = (err && err.message) || '系统错误';
+    const statusCode = (err.output && err.output.statusCode) || 500;
+    const errorMsg = (err.output && err.output.payload && err.output.payload.error) || err.message;
+    res.status(statusCode).json({
+        code: CODE_ERROR,
+        msg,
+        error: statusCode,
+        errorMsg
+    });
+
+});
+
+module.exports = router
