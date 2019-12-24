8eade52c6c37bbaaaab08f4c24179a6c7003978b
boye <boye_1990@aliyun.com>
2019-12-24 13:45:36 +0800

    node路由添加注释

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const express = require('express');
 // 创建 express 应用
 const app = express();
 
-// 监听 / 路径的 get 请求
+/**
+ * 路由，应用如何响应请求的一中规则
+ * 规则主要分为两部分：
+ * 请求方法： get post ...
+ * 请求路径： /， /user， /.*fly$/....
+ * 例如： 响应 / 路径 的 post 请求
+ * app.post('/', function(req, res) {
+ *     res.send('hello node')
+ * })
+ */
+
+// 监听 / 路径的 get 请求： 响应 / 路径 的 get 请求
 app.get('/', function(req, res) {
     res.send('hello node')
 })
