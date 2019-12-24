140875b3dfa0b02f54103f6c92cc469e87bb697f
boye <boye_1990@aliyun.com>
2019-12-24 15:02:41 +0800

    在根目录中新建app.js文件作为项目入口，使用路由响应请求

--- app.js
+++ app.js
@@ -19,32 +19,6 @@ app.get('/', function(req, res) {
     res.send('hello node')
 })
 
-/**
- * 中间件是一个函数，在请求和响应周期中被顺序调用
- */
-// 创建 myLogger 中间件
-const myLogger = function (req, res, next) {
-    console.log('myLogger')
-    next()
-}
-
-/**
- * 创建异常处理中间件， 处理请求中产生的的异常
- * 使用时要注意两点
- * 第一， 参数一个不能少，否则回被视为普通中间件
- * 第二， 中间件需要在请求之后引用
- */
-const errorHandler = function (err, req, res, next) {
-    console.log('errorHandler...')
-    res.status(500)
-    res.send('down...')
-}
-
-
-// 使用中间件， 中间件需要在响应结束前被调用
-app.use(myLogger)
-app.use(errorHandler)
-
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {
     const { address, port } = server.address()
